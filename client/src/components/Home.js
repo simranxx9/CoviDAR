0d1e3e71fb01ccb947aa148cbf3a25a18b107d50
simranxx9 <simran7051329916@gmail.com>
2021-05-31 23:41:28 +0530

    added initial home css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import '../css/Home.css'
 class Home extends Component {
     state = {
         data: ''
@@ -7,7 +7,7 @@ class Home extends Component {
     async getCovidData() {
         const res = await fetch(`https://api.covid19india.org/data.json`);
         const resData = await res.json()
-        console.log(resData.statewise)
+        // console.log(resData.statewise)
         this.setState({
             data: resData.statewise
         })
@@ -21,14 +21,23 @@ class Home extends Component {
         return (
             <div>
                 Home COvid
-                {this.state.data && this.state.data.map((res, ind) => {
-                    return (
-                        <li key={ind}>
-                            {res.state === "Total" &&
-                                <h2>{res.active}</h2>}
-                        </li>
-                    )
+                {this.state.data && this.state.data.filter(res => res.state === "Total").map((res, ind) => {
+                    return (<h2 key={ind}>{res.lastupdatedtime}</h2>)
                 })}
+                {this.state.data && this.state.data.filter(res => res.state === "Total").
+                    map((res, ind) => {
+
+                        return (
+                            <ul className="data-cards" key={ind}>
+
+                                <li className="data-details one" style={{ color: "blue" }}>active <br /><br />{res.active}</li>
+                                <li className="data-details two" style={{ color: "red" }}>confirmed <br /><br />{res.confirmed}</li>
+                                <li className="data-details three" style={{ color: "#888" }}>deaths <br /><br />{res.deaths}</li>
+                                <li className="data-details four" style={{ color: "green" }}>recovered <br /><br />{res.recovered}</li>
+                            </ul>
+                        )
+
+                    })}
             </div>
         )
     }
