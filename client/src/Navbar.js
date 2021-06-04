8e8af5c4ee7abc7f32e60c3babddee781a4dfe94
simranxx9 <simran7051329916@gmail.com>
2021-06-04 23:25:53 +0530

    added navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,87 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import './css/Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './css/Navbar.css';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">CORONA</Link>
-                </li>
-                <li>
-                    <Link to="/news">NEWS</Link>
-                </li>
-                <li>
-                    <Link to="/coviindia">Covid India</Link>
-                </li>
-                <li>
-                    <Link to="/faqs">FAQS</Link>
-                </li>
-            </ul>
-        </div>
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container '>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+
+
+                        {/* <img src={imag} width="210px" /> */}
+            LOGO
+
+
+
+          </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+              </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/news'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                News
+              </Link>
+                        </li>
+
+                        <li className='nav-item'>
+                            <Link
+                                to='/coviindia'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                CoviIndia
+              </Link>
+                        </li>
+
+
+                        <li className='nav-item'>
+                            <Link
+                                to='/query'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Query
+              </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/faqs'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                FAQS
+              </Link>
+                        </li>
+
+
+
+                    </ul>
+
+                </div>
+
+            </nav>
+        </>
     )
 }
 
