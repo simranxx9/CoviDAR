945d45fcba0ddfd666ad23db9a45e877dbdeeb0b
simranxx9 <simran7051329916@gmail.com>
2021-06-07 23:31:04 +0530

    added form query

--- client/src/components/Faqs.js
+++ client/src/components/Faqs.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import '../css/Faqs.css'
 import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Forms from './Forms'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -12,7 +13,7 @@ export class Faqs extends Component {
             { 'key': 2, 'question': "Why it is coronavirus", 'answer': "These viruses are called coronaviruses because the virus is visually characterized by fringed projections surrounding it, similar to rays and flares of the plasma aura around the sun, known as a solar corona." },
             { 'key': 3, 'question': "Why SARS-Cov-2", 'answer': "The International Committee on Taxonomy of Viruses (ICTV) who generally names the viruses termed it as SARS-Cov-2 because the virus is genetically related to the coronavirus that caused the SARS outbreak in 2003. Despite this genetic link, the viruses are distinct from each other." },
             { 'key': 7, 'question': "Why it is Covid19", 'answer': "The first case of corona virus was seen in Wuhan,China in December,2019" },
-            { 'key': 8, 'question': "What are the symptoms of Covid19", 'answer': "fine" },
+            { 'key': 8, 'question': "What are the symptoms of Covid19", 'answer': "Symtoms include fever, cough, cold, loss of smelling sense, breathing difficulty, and fatigueness." },
             { 'key': 4, 'question': "how are you", 'answer': "fine" },
             { 'key': 5, 'question': "how are you", 'answer': "fine" },
             { 'key': 6, 'question': "how are you", 'answer': "fine" }
@@ -33,54 +34,61 @@ export class Faqs extends Component {
     }
     render() {
         return (
-            <div>
-                <h1 className="head-box" style={{ marginTop: "50px" }}>Search your question</h1>
-                <div className="search-box" style={{ marginTop: "40px" }}>
-                    <i className="fa fa-search" aria-hidden="true"></i>
-                    <input type="text" className="input-search"
-                        onChange={this.handleChange} id="searchState"
-                    />
+            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
 
+                {/* form-container */}
+                <Forms />
+
+                <div className="faqs-container">
+                    <h1 className="head-box" style={{ color: "#50c878", padding: "10px", margin: "0", fontSize: "16px" }}>Search your question</h1>
+                    <div className="search-box">
+                        <i className="fa fa-search" aria-hidden="true"></i>
+                        <input type="text" className="input-search"
+                            onChange={this.handleChange} id="searchState"
+                        />
+
+                    </div>
+                    <div className="container-fluid">
+                        <h2 className="faqs-name">Frequently Asked Questions(FAQS)</h2>
+                    </div>
+
+
+                    {this.state.faqs.filter((res) => {
+                        if (res.question.toLowerCase().includes(this.state.searchState.toLowerCase())) {
+                            return res;
+
+                        }
+                        else if (this.state.searchState === "") {
+                            return res;
+                        }
+
+                    })
+                        .map((res, ind) => {
+                            return (
+
+                                <Accordion key={res.key}>
+                                    <Card>
+                                        <Accordion.Toggle eventKey={res.key} style={{ background: "#fff" }}>
+                                            <div style={{ textAlign: "left", padding: "10px" }} >
+
+                                                <h3><span style={{ color: "#000", fontWeight: "bold " }}>
+                                                    Q.{' '}</span><span style={{ color: "#50c878" }}>{res.question}</span>
+                                                    <span style={{ color: "#ff0000", fontWeight: "bold " }}>{' '}?</span>
+                                                </h3>
+                                            </div>
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey={res.key}>
+                                            <div className='panel'>
+
+                                                <p>{res.answer}</p>
+                                            </div>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            )
+                        })}
                 </div>
-                <div className="container-fluid">
-                    <h3 className="faqs-name">Frequently Asked Questions(FAQS)</h3>
-                </div>
 
-
-                {this.state.faqs.filter((res) => {
-                    if (res.question.toLowerCase().includes(this.state.searchState.toLowerCase())) {
-                        return res;
-
-                    }
-                    else if (this.state.searchState === "") {
-                        return res;
-                    }
-
-                })
-                    .map((res, ind) => {
-                        return (
-
-                            <Accordion key={res.key}>
-                                <Card>
-                                    <Accordion.Toggle eventKey={res.key} style={{ background: "#fff" }}>
-                                        <div style={{ textAlign: "left", padding: "10px" }} >
-
-                                            <h3><span style={{ color: "#000", fontWeight: "bold " }}>
-                                                Q.{' '}</span><span style={{ color: "#50c878" }}>{res.question}</span>
-                                                <span style={{ color: "#ff0000", fontWeight: "bold " }}>{' '}?</span>
-                                            </h3>
-                                        </div>
-                                    </Accordion.Toggle>
-                                    <Accordion.Collapse eventKey={res.key}>
-                                        <div className='panel'>
-
-                                            <p>{res.answer}</p>
-                                        </div>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        )
-                    })}
             </div>
         )
     }
