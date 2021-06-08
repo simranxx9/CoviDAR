47f443cf021097bc9dda599585ac029f8afff50b
simranxx9 <simran7051329916@gmail.com>
2021-06-08 13:17:36 +0530

    added form

--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,60 +1,83 @@
-import React from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { Component } from 'react';
 import '../css/Faqs.css'
 
-const Forms = (props) => {
-    return (
-        <Form className="forms-container">
-            <h1 className="head-box" style={{ color: "#50c878", padding: "10px", margin: "0", fontSize: "16px" }}>Ask Your Query!</h1>
-            <Row form>
-                <Col md={6}>
-                    <FormGroup>
-                        <Label for="exampleEmail">Email</Label>
-                        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                    </FormGroup>
-                </Col>
-                <Col md={6}>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                    </FormGroup>
-                </Col>
-            </Row>
-            <FormGroup>
-                <Label for="exampleAddress">Address</Label>
-                <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleAddress2">Address 2</Label>
-                <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor" />
-            </FormGroup>
-            <Row form>
-                <Col md={6}>
-                    <FormGroup>
-                        <Label for="exampleCity">City</Label>
-                        <Input type="text" name="city" id="exampleCity" />
-                    </FormGroup>
-                </Col>
-                <Col md={4}>
-                    <FormGroup>
-                        <Label for="exampleState">State</Label>
-                        <Input type="text" name="state" id="exampleState" />
-                    </FormGroup>
-                </Col>
-                <Col md={2}>
-                    <FormGroup>
-                        <Label for="exampleZip">Zip</Label>
-                        <Input type="text" name="zip" id="exampleZip" />
-                    </FormGroup>
-                </Col>
-            </Row>
-            <FormGroup check>
-                <Input type="checkbox" name="check" id="exampleCheck" />
-                <Label for="exampleCheck" check>Check me out</Label>
-            </FormGroup>
-            <Button>Submit</Button>
-        </Form>
-    );
+class Forms extends Component {
+    state = {
+        name: "",
+        query: "",
+        email: "",
+        mobileNumber: "",
+        address: ""
+    }
+    handleChange = (e) => {
+        this.setState(
+            {
+                [e.target.id]: e.target.value
+            }
+        )
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div className="forms-container">
+
+                <form className="white"
+                    onSubmit={this.handleSubmit}
+                >
+                    {/* <h5 className="grey-text text-darken-3">Sign Up</h5> */}
+                    <h1 className="head-box" style={{ color: "#50c878", padding: "10px", margin: "10px auto", fontSize: "16px" }}>Ask your Query!</h1>
+
+                    <div className="input-field">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name"
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="input-field">
+                        <label htmlFor="query" style={{ paddingLeft: "10px", marginBottom: "10px" }}>Query</label>
+                        <textarea id="query" style={{ padding: "10px", width: "300px", height: "200px" }}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email"
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+
+                    <div className="input-field">
+                        <label htmlFor="mobileNumber">Number</label>
+                        <input type="number" id="mobileNumber" min="10" max="10"
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="address">Address</label>
+                        <input type="text" id="address"
+                            onChange={this.handleChange}
+
+                        />
+                    </div>
+
+                    <div className="input-field">
+                        <button className="btn pink lighten-1 z-depth-0">Submit</button>
+                    </div>
+
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Forms;
