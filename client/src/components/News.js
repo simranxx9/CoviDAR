83143dfa57d0da89a3749851ef9d7d5d8699ca78
simranxx9 <simran7051329916@gmail.com>
2021-06-25 23:38:02 +0530

    styled news

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,38 +1,56 @@
 import React, { Component } from 'react'
+import Loading from './Loading'
 
 export class News extends Component {
     state = {
-        data: '',
+        news: '',
 
     }
-    getCovidData() {
-        fetch("https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&media=True", {
+    async getCovidData() {
+        const resNews = await fetch("https://coronavirus-smartable.p.rapidapi.com/news/v1/US/", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "cf20b9aaf7msh6ca24058bbdb5aap1abc80jsn5fcd4f5a5c31",
-                "x-rapidapi-host": "covid-19-news.p.rapidapi.com"
+                "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com"
             }
         })
-            .then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.error(err);
-            });
+        const res = await resNews.json()
+
+        this.setState({
+            news: res.news
+        })
+        console.log(this.state.news)
 
     }
     componentDidMount() {
 
         this.getCovidData()
+        console.log(this.state.news)
     }
 
     render() {
-        return (
-            <div>
-                News
-            </div>
-        )
+        if (this.state.news) {
+            return (
+                <div>
+                    {this.state.news && this.state.news.map((res, ind) => {
+                        return (
+                            <ul key={ind}>
+                                <li>{res.title}</li>
+                                <li>{res.excerpt}</li>
+                                <a href={res.webUrl}>For more</a>
+                            </ul>
+                        )
+                    })}
+                </div>
+            )
+        }
+        else {
+            return (
+                <Loading />
+            )
+        }
     }
+
 }
 
 export default News
